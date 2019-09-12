68dc4b962035951ed79af15151ff58c4f99661d3
Arjun Nair <nair.arjun47@yahoo.com>
2019-09-12 06:09:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |choice|
-    if counter == "X" || counter == "O"
+    if choice == "X" || choice == "O"
       counter += 1
     end
   return counter
